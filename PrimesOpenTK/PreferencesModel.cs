a1f2fb037faef0f8f312eb357abd5aa7341325ff
DESKTOP-SQK3IRP\Raix <paw.kanarek@gmail.com>
2020-02-01 15:16:56 +0100

    Cleanup

---
+++ PrimesOpenTK/PreferencesModel.cs
@@ -0,0 +1,11 @@
+
+namespace PrimesOpentTK
+{
+    public class PreferencesModel
+    {
+        public int WindowTop { get; set; } = 100;
+        public int WindowLeft { get; set; } = 100;
+        public int WindowHeight { get; set; } = 800;
+        public int WindowWidth { get; set; } = 1200;
+    }
+}
